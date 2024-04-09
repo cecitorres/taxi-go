0de8da25863920ef5bc39ab5aaa2869da4447652
Cecilia Torres <cecilia.torres@htech.com>
2024-04-08 17:22:05 -0700

    fix lon, lat order

--- composables/useUserLocation.js
+++ composables/useUserLocation.js
@@ -8,11 +8,11 @@ export default function () {
     const getUserLocation = async () => {
         if (process.env.NODE_ENV === "development") {
             // Hardcode coords for fake position
-            const lat = -100.1865;
-            const lon = 25.6718;
+            const lon = -100.1865;
+            const lat = 25.6718;
             // set fake waiting time of 5 seconds before returning values with timeout
             await new Promise((resolve) => setTimeout(resolve, 5000));
-            return [lat, lon];
+            return [lon, lat];
         }
         // GPS location
         if ("geolocation" in navigator) {
@@ -21,7 +21,7 @@ export default function () {
                 const position = await getCurrentPosition();
                 const lat = position.coords.latitude;
                 const lon = position.coords.longitude;
-                return [lat, lon];
+                return [lon, lat];
             } catch (err) {
                 throw Error("Error al obtener tu ubicacion");
             } finally {
